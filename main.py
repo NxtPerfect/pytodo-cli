edf1e4df28b0dd939b44971ba057052873997b2d
NxtPerfect <alakaxan@protonmail.com>
2023-04-28 23:01:31 +0200

    Preparing structure for code

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import argparse
+import datetime
+import json
+import os
+import colorama
+import tabulate
 
+class Cli():
+    pass
+
+class Task():
+    pass
+
+class FileManager():
+    pass
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    run()
