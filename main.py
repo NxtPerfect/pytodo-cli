668b72497112a8ea3906beae492e359f4b4f0d4c
NxtPerfect <alakaxan@protonmail.com>
2023-05-12 14:07:52 +0200

    Restructuring the program

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import argparse
+import config
 
+
+class Cli():
+    pass
+
+
+class ArgParser():
+    def __init__(self):
+        self.parser = argparse.ArgParser(
+            prog='Pytodo',
+            description='Simple python program to organize your work, add and complete tasks, set due date',)
+        self.parser.add_argument('taskname')
+        self.parser.add_argument('-c', '--complete')
+
+
+class Config():
+    pass
+
+
+def run():
+    argparse
+    config
